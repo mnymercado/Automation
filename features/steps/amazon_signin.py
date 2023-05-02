2ef5f876b40cd1c7f2098dadcd3e63c1c81a4e71
Nicole Mercado <nicolemercado@192.168.1.2>
2023-05-02 11:18:26 +0800

    Added a scenario if Cart is empty

--- features/steps/amazon_signin.py
+++ features/steps/amazon_signin.py
@@ -15,7 +15,7 @@ def input_text(context, name):
 
 @then('Verify amazon logo, continue button, Need help, Forgot Password, Other issues with Sign-In, Create you Amazon account button and {sign_in} text is shown')
 def verify_signin(context, sign_in):
-    actual_result = context.driver.find_element(By.XPATH, "//h1[text()]").text
+    actual_result = context.driver.find_element(By.XPATH, "//h1[@class='a-spacing-small']").text
     assert actual_result == sign_in, f'Expected {sign_in}, but {actual_result} is shown'
     assert context.driver.find_element(By.XPATH, "//i[@class='a-icon a-icon-logo']"), f'Amazon logo not shown' #Amazon Logo
     assert context.driver.find_element(By.ID, "continue"), f'Continue button not shown' #Continue button
@@ -24,3 +24,11 @@ def verify_signin(context, sign_in):
     assert context.driver.find_element(By.ID, "ap-other-signin-issues-link"), f'Other issues in Signin link not found' # Other issues with Sign-In link
     assert context.driver.find_element(By.ID, "createAccountSubmit"), f'Create Amazon account button not shown' # Create your Amazon account button
 
+@when('Click Cart')
+def click_cart(context):
+    context.driver.find_element(By.CSS_SELECTOR, 'span.nav-cart-icon').click()
+
+@then('Verify {cart_status}')
+def verify_cart(context, cart_status):
+    actual_result = context.driver.find_element(By.CSS_SELECTOR, 'div.sc-your-amazon-cart-is-empty').text
+    assert actual_result == cart_status, f'Expected result is {cart_status}, but {actual_result} is shown'
